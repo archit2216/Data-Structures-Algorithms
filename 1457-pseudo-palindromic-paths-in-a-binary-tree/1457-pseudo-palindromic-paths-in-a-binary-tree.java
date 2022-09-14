03ebacd7ed6e0791c5836094b4353908016f2036
Archit Sharma <65528628+archit2216@users.noreply.github.com>
2022-09-14 12:45:52 +0530

    Time: 75 ms (27.19%), Space: 112.7 MB (27.48%) - LeetHub

--- 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.java
+++ 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.java
@@ -16,21 +16,16 @@
 class Solution {
     ArrayList<ArrayList<Integer>> ans;
     int count;
-    public void getPaths(TreeNode root,HashMap<Integer,Integer> hm){
+    public void getPaths(TreeNode root,int[] map){
         if(root==null){
             return;
         }
         if(root.left==null && root.right==null){
-            HashMap<Integer,Integer> newHm=new HashMap<>(hm);
-            if(newHm.containsKey(root.val)){
-                int val=newHm.get(root.val);
-                newHm.put(root.val,val+1);
-            }else{
-                newHm.put(root.val,1);
-            }
+            int[] arr=map.clone();
+            arr[root.val]++;
             int oddvals=0;
-            for(int k:newHm.keySet()){
-                if(newHm.get(k)%2!=0){
+            for(int i=0;i<arr.length;i++){
+                if(arr[i]%2!=0){
                     oddvals++;
                 }
             }
@@ -40,21 +35,16 @@ class Solution {
             return;
         }
         
-        if(hm.containsKey(root.val)){
-            int val=hm.get(root.val);
-            hm.put(root.val,val+1);
-        }else{
-            hm.put(root.val,1);
-        }
-        getPaths(root.left,hm);
-        getPaths(root.right,hm);
-        int val=hm.get(root.val);
-        hm.put(root.val,val-1);
+        map[root.val]++;
+        getPaths(root.left,map);
+        getPaths(root.right,map);
+        map[root.val]--;
     }
     public int pseudoPalindromicPaths (TreeNode root) {
         ans=new ArrayList<>();
         count=0;
-        getPaths(root,new HashMap<>());
+        int[] map=new int[10];
+        getPaths(root,map);
         
         return count;
     }
