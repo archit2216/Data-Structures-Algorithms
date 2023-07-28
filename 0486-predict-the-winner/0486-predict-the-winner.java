143d47d5e580488cc759dd45d35772436c9d34b9
Archit Sharma <65528628+archit2216@users.noreply.github.com>
2023-07-28 13:59:30 +0530

    Time: 0 ms (100.00%), Space: 40.1 MB (53.98%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.java
+++ 0486-predict-the-winner/0486-predict-the-winner.java
@@ -1,19 +1,22 @@
 class Solution {
-    public int solve(int i,int j,int[] nums){
+    public int solve(int i,int j,int[] nums,Integer[][] dp){
         if(i>j){
             return 0;
         }
-        
-        int incI=nums[i]+Math.min(solve(i+2,j,nums),solve(i+1,j-1,nums));
-        int incJ=nums[j]+Math.min(solve(i,j-2,nums),solve(i+1,j-1,nums));
-        return Math.max(incI,incJ);
+        if(dp[i][j]!=null){
+            return dp[i][j];
+        }
+        int incI=nums[i]+Math.min(solve(i+2,j,nums,dp),solve(i+1,j-1,nums,dp));
+        int incJ=nums[j]+Math.min(solve(i,j-2,nums,dp),solve(i+1,j-1,nums,dp));
+        return dp[i][j]=Math.max(incI,incJ);
     }
     public boolean PredictTheWinner(int[] nums) {
         int sum=0;
         for(int i=0;i<nums.length;i++){
             sum+=nums[i];
         }
-        int s1=solve(0,nums.length-1,nums);
+        Integer[][] dp=new Integer[nums.length][nums.length];
+        int s1=solve(0,nums.length-1,nums,dp);
         if(sum-s1>s1){
             return false;
         }
