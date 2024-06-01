c9e3658b8d1aadd58e1f4588681441786ec1c5dc
Archit Sharma <65528628+archit2216@users.noreply.github.com>
2024-06-01 23:34:03 +0530

    Time: 0 ms (100.00%), Space: 44 MB (90.65%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
@@ -17,11 +17,9 @@ public class Solution {
         
         ListNode slow=head;
         ListNode fast=head;
-        
         while(fast!=null && fast.next!=null){
             slow=slow.next;
             fast=fast.next.next;
-            
             if(slow==fast){
                 break;
             }
